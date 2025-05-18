8a5654600178633dd3baee5d152a27634de72ec2
BuraYu <burak.yueksel@hotmail.com>
2025-05-18 21:43:35 +0200

    initial commit, add favicon

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="h-screen place-content-center grid">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
