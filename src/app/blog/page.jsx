ba7d47fd3558b8b10f66a623395ec84b8fe69b52
BuraYu <burak.yueksel@hotmail.com>
2025-05-24 20:50:59 +0200

    feat: add blog page

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,7 +1,81 @@
+// pages/blog.js
+import React from "react";
+
 const Blog = () => {
+  const blogPosts = [
+    {
+      id: 1,
+      title: "Titel #1",
+      date: "October 1, 2023",
+      shortText:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat vero, totam iure excepturi soluta rerum odit. Iusto similique esse iure?",
+      image: "./images/blog/customer_experience.jpg",
+    },
+    {
+      id: 2,
+      title: "Titel #2",
+      date: "September 25, 2023",
+      shortText:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat vero, totam iure excepturi soluta rerum odit. Iusto similique esse iure?",
+      image: "./images/blog/developement_packaging.jpg",
+    },
+    {
+      id: 3,
+      title: "Titel #3",
+      date: "September 15, 2023",
+      shortText:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat vero, totam iure excepturi soluta rerum odit. Iusto similique esse iure?",
+      image: "./images/blog/innovatice_package.jpg",
+    },
+    {
+      id: 4,
+      title: "Frohe Pfingsten!",
+      date: "Juni 8, 2025",
+      shortText:
+        "Wir wünschen allen ein frohes und erholsames Pfingstfest! Möge diese besondere Zeit voller Frieden, Hoffnung und gemeinsamer Momente mit euren Liebsten sein.",
+      image: "./images/blog/pfingsten_gruesse.webp",
+    },
+  ];
+
   return (
-    <div>
-      <h1>Blog</h1>
+    <div className="bg-[#F9FAFB] py-16 px-6 h-full">
+      <div className="max-w-4xl mx-auto pb-0">
+        <h1 className="text-4xl font-bold text-gray-900 text-center mb-12">
+          Blog
+        </h1>
+
+        <div className="space-y-4">
+          {blogPosts
+            .slice()
+            .reverse()
+            .map((post, index) => (
+              <div
+                key={post.id}
+                className={`flex flex-col md:flex-row gap-8 ${
+                  index !== blogPosts.length - 1
+                    ? "border-b border-gray-200 pb-8 sm:border-none"
+                    : ""
+                }`}
+              >
+                <div className="w-full md:w-1/2">
+                  <img
+                    src={post.image}
+                    alt={post.title}
+                    className="w-full h-auto"
+                  />
+                </div>
+
+                <div className="w-full md:w-1/2">
+                  <h2 className="text-2xl font-semibold text-gray-800 mb-2">
+                    {post.title}
+                  </h2>
+                  <p className="text-gray-500 text-sm mb-4">{post.date}</p>
+                  <p className="text-gray-700 text-lg">{post.shortText}</p>
+                </div>
+              </div>
+            ))}
+        </div>
+      </div>
     </div>
   );
 };
